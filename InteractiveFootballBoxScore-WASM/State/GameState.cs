8a64bb66554b59c2822bec0d2e74dad520eaf6f1
maotterson <otterson.2@gmail.com>
2022-02-10 23:07:33 -0500

    create game state service, refactor -> extract game data in service instead of index.razor

---
+++ InteractiveFootballBoxScore-WASM/State/GameState.cs
@@ -0,0 +1,55 @@
+﻿using InteractiveFootballBoxScore_WASM.PbpParser.Models;
+using PlayByPlayParser;
+using PlayByPlayParser.Models;
+
+namespace InteractiveFootballBoxScore_WASM.State
+{
+    public class GameState
+    {
+        public Game CurrentGame { get; private set; }
+
+        public event Action OnChange;
+
+        public async Task ProcessGameDataFromHTTP(HttpResponseMessage response)
+        {
+            CurrentGame = new Game()
+            {
+                PlayList = new List<Play>()
+            };
+
+            Stream responseStream = await response.Content.ReadAsStreamAsync();
+            using (var reader = new StreamReader(responseStream))
+            {
+                //skip header line
+                reader.ReadLine();
+
+                int index = 0;
+                while (reader.EndOfStream == false)
+                {
+                    index++;
+                    string[] data = reader.ReadLine().Split(",");
+                    Play currentPlay = new Play
+                    {
+                        PlayIndex = index,
+                        Quarter = data[0],
+                        Time = data[1],
+                        Down = data[2],
+                        ToGo = data[3],
+                        Location = data[4],
+                        Summary = data[5],
+                        PlayEvent = PlayEventFactory.ExtractPlayEvent(data[5])
+                    };
+                    CurrentGame.PlayList.Add(currentPlay);
+                }
+            }
+        }
+
+        public void SetCurrentGame(Game game)
+        {
+            CurrentGame = game;
+            NotifyStateChanged();
+        }
+
+        private void NotifyStateChanged() => OnChange?.Invoke();
+    }
+}
