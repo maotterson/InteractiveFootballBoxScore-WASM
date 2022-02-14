dd314ecbc22f2cedee6a8abca8b6b820d6470f68
maotterson <otterson.2@gmail.com>
2022-02-14 01:43:46 -0500

    add toggle for sample-data and exclude from repo

--- InteractiveFootballBoxScore-WASM/PbpParser/Libraries/TeamLibrary.cs
+++ InteractiveFootballBoxScore-WASM/PbpParser/Libraries/TeamLibrary.cs
@@ -12,6 +12,8 @@ namespace InteractiveFootballBoxScore_WASM.PbpParser.Libraries
             { TeamAcronym.ARI, new Team{ Name = "Arizona", Nickname = "Cardinals", PrimaryColor = "#97233F", SecondaryColor = "#000000", Acronym = TeamAcronym.ARI} },
             { TeamAcronym.GNB, new Team{ Name = "Green Bay", Nickname = "Packers", PrimaryColor = "#203731", SecondaryColor = "#FFB612", Acronym = TeamAcronym.GNB} },
             { TeamAcronym.SFO, new Team{ Name = "San Francisco", Nickname = "49ers", PrimaryColor = "#AA0000", SecondaryColor = "#B3995D", Acronym = TeamAcronym.SFO} },
+            { TeamAcronym.CLE, new Team{ Name = "Cleveland", Nickname = "Browns", PrimaryColor = "#311D00", SecondaryColor = "#ff3c00", Acronym = TeamAcronym.SFO} },
+            { TeamAcronym.CHI, new Team{ Name = "Chicago", Nickname = "Bears", PrimaryColor = "#0B162A", SecondaryColor = "#c83803", Acronym = TeamAcronym.SFO} },
         };
     }
 }
