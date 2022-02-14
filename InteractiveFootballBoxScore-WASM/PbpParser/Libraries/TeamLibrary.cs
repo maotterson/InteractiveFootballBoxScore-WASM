77730cea6bb22e4d5567b1a66ce510d0a9697784
maotterson <otterson.2@gmail.com>
2022-02-13 23:29:10 -0500

    display only nickname on endzone

--- InteractiveFootballBoxScore-WASM/PbpParser/Libraries/TeamLibrary.cs
+++ InteractiveFootballBoxScore-WASM/PbpParser/Libraries/TeamLibrary.cs
@@ -9,9 +9,9 @@ namespace InteractiveFootballBoxScore_WASM.PbpParser.Libraries
     {
         public static Dictionary<TeamAcronym, Team> teamDictionary = new Dictionary<TeamAcronym, Team>
         {
-            { TeamAcronym.ARI, new Team{ Name = "Arizona Cardinals", PrimaryColor = "#97233F", SecondaryColor = "#000000", Acronym = TeamAcronym.ARI} },
-            { TeamAcronym.GNB, new Team{ Name = "Green Bay Packers", PrimaryColor = "#203731", SecondaryColor = "#FFB612", Acronym = TeamAcronym.GNB} },
-            { TeamAcronym.SFO, new Team{ Name = "San Francisco 49ers", PrimaryColor = "#AA0000", SecondaryColor = "#B3995D", Acronym = TeamAcronym.SFO} },
+            { TeamAcronym.ARI, new Team{ Name = "Arizona", Nickname = "Cardinals", PrimaryColor = "#97233F", SecondaryColor = "#000000", Acronym = TeamAcronym.ARI} },
+            { TeamAcronym.GNB, new Team{ Name = "Green Bay", Nickname = "Packers", PrimaryColor = "#203731", SecondaryColor = "#FFB612", Acronym = TeamAcronym.GNB} },
+            { TeamAcronym.SFO, new Team{ Name = "San Francisco", Nickname = "49ers", PrimaryColor = "#AA0000", SecondaryColor = "#B3995D", Acronym = TeamAcronym.SFO} },
         };
     }
 }
--- InteractiveFootballBoxScore-WASM/PbpParser/Models/Team.cs
+++ InteractiveFootballBoxScore-WASM/PbpParser/Models/Team.cs
@@ -5,6 +5,7 @@ namespace InteractiveFootballBoxScore_WASM.PbpParser.Models
     public class Team
     {
         public string? Name { get; set; }
+        public string? Nickname { get; set; }
         public string? PrimaryColor { get; set; }
         public string? SecondaryColor { get; set; }
         public TeamAcronym Acronym { get; set; }
